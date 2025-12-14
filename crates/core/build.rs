63dfea0bbd7bbe1bb08e698c29058c76ad68ad5c
Alessio Pelliccione <66967361+alessiopelliccione@users.noreply.github.com>
2025-12-15 00:26:53 +0100

    build: conditionally link `advapi32` and `user32` libraries for Windows builds

--- crates/core/build.rs
+++ crates/core/build.rs
@@ -2,4 +2,9 @@ extern crate napi_build;
 
 fn main() {
     napi_build::setup();
+
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        println!("cargo:rustc-link-lib=advapi32");
+        println!("cargo:rustc-link-lib=user32");
+    }
 }
